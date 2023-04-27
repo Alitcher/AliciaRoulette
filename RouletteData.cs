7e417713ee16e592808168c48cfbc9046e0f8bc4
alicia22nzd <alicia22nzd@gmail.com>
2023-04-27 23:13:37 +0300

    TCP implementation

---
+++ RouletteData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Roulette
+{
+    public class RouletteData
+    {
+        public string Qualifier { get; set; }
+        public Data Data { get; set; }
+    }
+
+    public class Data
+    {
+        public int WinningNumber { get; set; }
+    }
+}
---
+++ RouletteTcpListener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using Newtonsoft.Json;
+using Roulette;
+
+public class RouletteTcpListener
+{
+    public event EventHandler<RouletteData> OnDataReceived;
+
+    public async void StartListening()
+    {
+        int port = 5000;
+        TcpListener listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+
+        while (true)
+        {
+            TcpClient client = await listener.AcceptTcpClientAsync();
+            StreamReader reader = new StreamReader(client.GetStream());
+            string jsonString = await reader.ReadToEndAsync();
+
+            RouletteData rouletteData = JsonConvert.DeserializeObject<RouletteData>(jsonString);
+
+            OnDataReceived?.Invoke(this, rouletteData);
+        }
+    }
+}
